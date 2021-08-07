2c974a6478922e5776f89c0c8599a18fa74c4820
Ebube <dbupanyiam@gmail.com>
2021-08-07 12:22:07 +0100

    Changed mongodb connection string

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/sass-80", {
+require("dotenv").config();
+
+// local-url-connection-string -- "mongodb://127.0.0.1:27017/sass-80"
+
+mongoose.connect(process.env.MONGODB_CONNECTION_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
