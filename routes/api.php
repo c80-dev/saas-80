a9dc54e02ffba66adca8c214e052f91cbc7b56ad
Akposieyefa <orutu1@gmail.com>
2021-09-17 13:35:18 +0100

    update

--- routes/api.php
+++ routes/api.php
@@ -14,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
-
 Route::group(['middleware' => 'api', 'prefix' => 'v0.01'], function ($router) {
 
        Route::post('/login', [App\Http\Controllers\Api\AuthController::class, 'login']);
