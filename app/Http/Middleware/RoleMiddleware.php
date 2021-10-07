5ad8a714e1bec38e702826ff02953d6c3bf772e9
Akposieyefa <orutu1@gmail.com>
2021-10-07 14:23:31 +0100

    code update

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -16,14 +16,10 @@ class RoleMiddleware
      */
     public function handle($request, Closure $next, $role, $permission = null)
     {
-        if ($request->user()->hasRole('superadmin')) {
-            return $next($request);
-        }else {
-            if(!$request->user()->hasRole($role)) {
-                return response()->json([
-                    'message' => 'Sorry you are not allowed to access this page'
-                ], 400);
-            }
+        if(!$request->user()->hasRole($role)) {
+            return response()->json([
+                'message' => 'Sorry you are not allowed to access this page'
+            ], 400);
         }
        
         if($permission !== null && !$request->user()->can($permission)) {
